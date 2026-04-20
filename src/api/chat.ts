391798a19b87a3ad3c19dde8a1454cfe7dc01fd1
Veljko Naumovic <veljko.naumovic@gmail.com>
2026-04-20 23:03:47 +0200

    Add x-user-id in headers od api call

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -1,11 +1,18 @@
 export const apiFetch = async (url: string, options: RequestInit = {}) => {
+	let userId = localStorage.getItem("userId");
+
+	if (!userId) {
+		userId = crypto.randomUUID();
+		localStorage.setItem("userId", userId);
+	}
+
 	const isBody = options.body !== undefined;
 
 	const res = await fetch(url, {
 		...options,
-		credentials: "include",
 		headers: {
 			...(isBody ? { "Content-Type": "application/json" } : {}),
+			"x-user-id": userId,
 			...(options.headers || {}),
 		},
 	});
