267c0cefbd72f2bae57bdb6faf840e20248dc760
Veljko Naumovic <veljko.naumovic@gmail.com>
2026-05-01 12:45:31 +0200

    Improve footer, add links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,17 @@
 import "./Footer.scss";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
 
 const Footer = () => {
 	return (
 		<footer className="footer">
 			<div className="container footer-content">
-				<p className="footer-text">
-					© {new Date().getFullYear()} Veljko Naumović
-				</p>
+				<div className="footer-left">
+					<p className="footer-text">
+						© {new Date().getFullYear()} Veljko Naumović
+					</p>
+					<p className="footer-sub">Built with React & TypeScript</p>
+				</div>
 
 				<div className="footer-links">
 					<a
@@ -14,14 +19,22 @@ const Footer = () => {
 						target="_blank"
 						rel="noopener noreferrer"
 					>
+						<FaGithub />
 						GitHub
 					</a>
+
 					<a
 						href="https://www.linkedin.com/in/veljko-naumovic-3370381a4/"
 						target="_blank"
 						rel="noopener noreferrer"
 					>
+						<FaLinkedin />
 						LinkedIn
+					</a>
+
+					<a href="mailto:veljko.naumovic@gmail.com?subject=Project inquiry&body=Hi Veljko,%0D%0A%0D%0A">
+						<HiOutlineMail />
+						Email
 					</a>
 				</div>
 			</div>
